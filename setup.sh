ba5a345cd30f9ebc78084b289dba94f8e4390c11
Iosif Rodopoulos <bonkeyzz10@gmail.com>
2021-04-05 19:06:49 +0300

    setup.sh: Download some nice plugins :)

--- setup.sh
+++ setup.sh
@@ -3,3 +3,15 @@
 # Move current zshrc to the home directory
 cp .zshrc ~/.zshrc
 
+# Make a plugins folder
+[ ! -d "/usr/share/zsh/plugins" ] && sudo mkdir /usr/share/zsh/plugins
+
+# Install zsh-syntax-highlighting plugin
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting /usr/share/zsh/plugins/zsh-syntax-highlighting
+
+# Install zsh-autosuggestions plugin
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions /usr/share/zsh/plugins/zsh-autosuggestions
+
+# Install zsh-abbr plugin
+sudo git clone https://github.com/olets/zsh-abbr /usr/share/zsh/plugins/zsh-abbr
+
