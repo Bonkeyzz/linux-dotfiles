00522aad007d0e724c3701a950653720aadbbffa
Iosif Rodopoulos <bonkeyzz10@gmail.com>
2021-04-06 17:49:54 +0300

    setup.sh: Ability to install vim-plug with some plugins i like.
    This will install some useful plugins which i like using.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Installing ZSH stuff..."
+
 # Remove existing dotfiles
 rm ~/.zshrc
 rm ~/.config/.aliasrc
@@ -9,6 +11,8 @@ cp zshrc ~/.zshrc
 
 # Copy current aliasrc to the .config directory
 cp aliasrc ~/.config/.aliasrc
+
+HOME_DIR="/home/$USER"
 
 PLUGINS_DIR="/usr/share/zsh/plugins"
 
@@ -22,3 +26,38 @@ PLUGINS_DIR="/usr/share/zsh/plugins"
 # Install zsh-autosuggestions plugin
 [ ! -d "$PLUGINS_DIR/zsh-autosuggestions" ] && \
 	sudo git clone https://github.com/zsh-users/zsh-autosuggestions $PLUGINS_DIR/zsh-autosuggestions || echo "Plugin: zsh-autosuggestions is already installed."
+
+echo "Installing neovim stuff..."
+
+# Neovim
+
+# Install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# Make directory for vim plugins
+
+VIM_PLUGGED="$HOME_DIR/.vim/plugged"
+NVIM_CONFIG="$HOME_DIR/.config/nvim"
+[ ! -d "$VIM_PLUGGED" ] && \
+	mkdir -pv "$VIM_PLUGGED" || echo "Directory: $VIM_PLUGGED exists."
+[ ! -d  "$NVIM_CONFIG" ] && \
+	mkdir -pv "$NVIM_CONFIG" || echo "Directory $NVIM_CONFIG exists."
+
+# Remove old vimrc
+rm ~/.vimrc
+
+# Copy init (that includes only the plugins to install) to home
+cp init_plugins.vim ~/.vimrc
+
+# Make a symlink
+ln -sf "$HOME_DIR/.vimrc" "$NVIM_CONFIG/init.vim"
+
+# Install all plugins
+nvim -c PlugInstall
+
+# Install the final config
+# rm ~/.vimrc
+cp init_final.vim ~/.vimrc
+
+echo "*** Done! ***"
